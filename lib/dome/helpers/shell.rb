daa6c4f7302621638cd1fcecd6cb52d8f2181046
Andrew Stangl <andrewstangl@gmail.com>
2018-01-16 15:59:36 +0200

    appease the copper

--- lib/dome/helpers/shell.rb
+++ lib/dome/helpers/shell.rb
@@ -4,11 +4,11 @@ module Dome
   module Shell
     def execute_command(command, failure_message)
       puts "About to execute command: #{command.colorize(:yellow)}"
-      Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
-        while line = stdout.gets
+      Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
+        while line == stdout.gets
           puts line
         end
-        while line = stderr.gets
+        while line == stderr.gets
           puts line
         end
         exit_status = wait_thr.value
