31b8c48095d659e32a1f0ffde1cfddc05ee4fc55
Mate Farkas <mate.farkas@itv.com>
2019-03-21 18:26:31 +0000

    Add Vault login.
    - Vault helper is loaded lazily as Gems can't have git-based dependencies

--- lib/dome/terraform.rb
+++ lib/dome/terraform.rb
@@ -100,6 +100,18 @@ module Dome
         @environment.invalid_environment_message unless @environment.valid_environment? environment
         @environment.unset_aws_keys
         @environment.aws_credentials
+        puts '--- Vault login ---'
+        case level
+        when 'secrets-init'
+          role = 'service_administrator'
+        when 'secrets-config'
+          role = 'content_administrator'
+        else
+          raise Dome::InvalidLevelError.new(level)
+        end
+        puts "[*] Logging in as: #{role}"
+        vault_login(role)
+        puts ''
       else
         raise Dome::InvalidLevelError.new(level)
       end
@@ -210,6 +222,20 @@ module Dome
       command         = 'terraform output'
       failure_message = 'something went wrong when printing TF output variables'
       execute_command(command, failure_message)
+    end
+
+    private
+
+    def vault_login(role)
+      begin
+        require 'vault/helper'
+      rescue LoadError
+        raise '[!] Failed to load vault/helper. Please add "gem \'hiera-vault\', git: \'git@github.com:ITV/hiera-vault\', ref: \'v1.0.0\'" or later to your Gemfile'.colorize(:red)
+      end
+      product = ENV['TF_VAR_product']
+      environment_name = @environment.environment
+      # TODO: Change to TCP8200 after port is open on the firewall
+      Vault::Helper.login(role, address: "https://secrets.#{environment_name}.#{product}.itv.com:8200")
     end
   end
 end
