c2efe2081b81f50006c825e8a92c92e09e0d4219
Mate Farkas <mate.farkas@itv.com>
2019-05-16 17:37:29 +0000

    Update test.

--- spec/secrets_spec.rb
+++ spec/secrets_spec.rb
@@ -15,31 +15,31 @@ describe Dome do
   context 'if config is missing from itv.yaml' do
     context 'outputs a debug message to STDOUT' do
       it 'when missing the parent key dome' do
-        allow(secrets.settings).to receive(:load_yaml).and_return({})
+        allow(secrets.settings).to receive(:parse).and_return({})
         expect { secrets.dome_config }.to output.to_stdout
       end
 
       it 'when missing the sub-key hiera_keys' do
         yaml = { 'dome' => { 'foo' => 'bar' } }
-        allow(secrets.settings).to receive(:load_yaml).and_return(yaml)
+        allow(secrets.settings).to receive(:parse).and_return(yaml)
         expect { secrets.hiera_keys_config }.to output.to_stdout
       end
 
       it 'when missing the sub-key certs' do
         yaml = { 'dome' => { 'foo' => 'bar' } }
-        allow(secrets.settings).to receive(:load_yaml).and_return(yaml)
+        allow(secrets.settings).to receive(:parse).and_return(yaml)
         expect { secrets.certs_config }.to output.to_stdout
       end
     end
 
     it 'does not set secret environment variables' do
-      allow(secrets.settings).to receive(:load_yaml).and_return({})
+      allow(secrets.settings).to receive(:parse).and_return({})
       expect(secrets.hiera).not_to receive(:secret_env_vars)
       secrets.secret_env_vars
     end
 
     it 'does not extract certificates' do
-      allow(secrets.settings).to receive(:load_yaml).and_return({})
+      allow(secrets.settings).to receive(:parse).and_return({})
       expect(secrets.hiera).not_to receive(:extract_certs)
       secrets.extract_certs
     end
@@ -52,14 +52,14 @@ describe Dome do
   context 'with valid config' do
     it 'sets secret environment variables' do
       yaml = { 'dome' => { 'hiera_keys' => { 'artifactory_password' => 'artifactory::root-readonly::password' } } }
-      allow(secrets.settings).to receive(:load_yaml).and_return(yaml)
+      allow(secrets.settings).to receive(:parse).and_return(yaml)
       expect(secrets.hiera).to receive(:secret_env_vars)
       secrets.secret_env_vars
     end
 
     it 'extracts certificates' do
       yaml = { 'dome' => { 'certs' => { 'id_rsa' => 'aws::ssh_privkey_content' } } }
-      allow(secrets.settings).to receive(:load_yaml).and_return(yaml)
+      allow(secrets.settings).to receive(:parse).and_return(yaml)
       expect(secrets.hiera).to receive(:extract_certs)
       secrets.extract_certs
     end
